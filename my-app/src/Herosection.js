b38538943c4f08c1ca8fac3ddd65368338ba2f4b
jashwanth0712 <jashwanth0712@gmail.com>
2023-03-29 21:58:00 +0530

    final

--- my-app/src/Herosection.js
+++ my-app/src/Herosection.js
@@ -4,7 +4,7 @@ import "./HeroSection.css";
 const HeroSection = () => {
   return (
     <section className="hero-section">
-      <div className="container">
+      <div className="containr">
         <div className="hero-content">
           <h2>Get Your Hearing Back on Track with Quietum Plus</h2>
           <p>
@@ -17,6 +17,7 @@ const HeroSection = () => {
           <a href="https://hop.clickbank.net/?affiliate=369369gr&vendor=socialsrep&pid=joblandingpage&ifso=280adayworkasalivechatassistant" className="btn-primary">Apply now</a>
 
         </div>
+        <img src="https://static.vecteezy.com/system/resources/previews/017/181/245/non_2x/live-chat-operator-semi-flat-color-character-editable-figure-full-body-person-on-white-simple-cartoon-style-illustration-for-web-graphic-design-and-animation-vector.jpg" style={{width:"50%"}}/> 
       </div>
     </section>
   );

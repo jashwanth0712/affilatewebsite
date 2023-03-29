3e5d3826bf22aeba3adb31c2aad257609be8236d
jashwanth0712 <jashwanth0712@gmail.com>
2023-03-29 23:05:02 +0530

    added google analytics

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,10 @@ import Header from "./Header";
 import FeaturesSection from "./FeaturesSection";
 import HeroSection from "./Herosection";
 
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('G-DEJRB6HVCS');
+ReactGA.pageview(window.location.pathname + window.location.search);
 const App = () => {
   return (
     <div className="app">
